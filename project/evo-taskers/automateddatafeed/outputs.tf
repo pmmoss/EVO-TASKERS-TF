c8e37c2dd14a032e69ea32f530cfb2609364f28b
Patrick Moss <patrick.moss@viking.com>
2025-10-17 17:08:06 -0400

    f

--- project/evo-taskers/automateddatafeed/outputs.tf
+++ project/evo-taskers/automateddatafeed/outputs.tf
@@ -5,23 +5,3 @@ output "environment" {
   value       = local.environment
 }
 
-output "app_service_name" {
-  description = "Name of the App Service"
-  value       = module.app_service.app_service_name
-}
-
-output "app_service_default_hostname" {
-  description = "Default hostname of the App Service"
-  value       = module.app_service.default_hostname
-}
-
-output "function_app_name" {
-  description = "Name of the Function App"
-  value       = module.windows_function_app.function_app_name
-}
-
-output "function_app_default_hostname" {
-  description = "Default hostname of the Function App"
-  value       = module.windows_function_app.default_hostname
-}
-
